27c8fde38a19b35d207db0260884b320882de2e4
Rakshu047 <poojaryrakshithn@gmail.com>
2023-11-13 10:30:03 +0530

    Created colors.py
    For managing cell colors and to prevent repetition of code

--- grid.py
+++ grid.py
@@ -13,18 +13,6 @@ class Grid:
             for col in range(self.num_cols):
                 print(self.grid[row][col], end = " ")
             print()
-
-    def get_cell_colors(self):
-        dark_grey = (26, 31, 40)
-        green = (47, 230, 23)
-        red = (232, 18, 18)
-        orange = (226, 116, 17)
-        yellow = (237, 234, 4)
-        purple = (116, 0, 247)
-        cyan = (21, 204, 209)
-        blue = (13, 64, 216)
-
-        return [dark_grey, green, red, orange, yellow, purple,cyan, blue]
     
     def draw(self, screen):
         for row in range(self.num_rows):
