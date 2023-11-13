17595d81b9d32c17872cc6e024f83549e0084dc5
Rakshu047 <poojaryrakshithn@gmail.com>
2023-11-13 12:32:33 +0530

    Moving the blocks
    >Create a file called game.py to manage all the elements of the game like blocks and grid

--- block.py
+++ block.py
@@ -1,4 +1,5 @@
 from colors import Colors
+from position import Position
 import pygame
 
 class Block:
@@ -6,11 +7,25 @@ class Block:
         self.id = id
         self.cells = {}
         self.cell_size = 30
+        self.row_offset = 0
+        self.col_offset = 0
         self.rotation_state = 0
         self.colors = Colors.get_cell_colors() 
 
-    def draw(self,screen):
+    def move(self,row,col):
+        self.row_offset += row
+        self.col_offset += col
+
+    def get_cell_position(self):
         tiles = self.cells[self.rotation_state]
+        moved_tiles = []
+        for pos in tiles:
+            pos = Position(pos.row + self.row_offset, pos.column + self.col_offset)
+            moved_tiles.append(pos)
+        return moved_tiles
+
+    def draw(self,screen):
+        tiles = self.get_cell_position()
         for tile in tiles:
             tile_rect = pygame.Rect(tile.column*self.cell_size +1, tile.row*self.cell_size +1, self.cell_size -1, self.cell_size -1)
             pygame.draw.rect(screen, self.colors[self.id], tile_rect)
---
+++ game.py
@@ -0,0 +1,43 @@
+from grid import Grid
+from blocks import *
+import random
+
+class Game:
+    def __init__(self):
+        self.grid = Grid()
+        self.blocks = [LBlock(),JBlock(),IBlock(),OBlock(),SBlock(),TBlock(),ZBlock()]
+        self.cur_block = self.get_random_block()
+        self.next_block = self.get_random_block()
+
+    def get_random_block(self):
+        if len(self.blocks) == 0:
+            self.blocks = [LBlock(),JBlock(),IBlock(),OBlock(),SBlock(),TBlock(),ZBlock()]
+        block = random.choice(self.blocks)
+        self.blocks.remove(block)
+        return block
+    
+    def move_left(self):
+        self.cur_block.move(0,-1)
+        if self.block_inside() == False:
+            self.cur_block.move(0,1)
+    
+    def move_right(self):
+        self.cur_block.move(0,1)
+        if self.block_inside() == False:
+            self.cur_block.move(0,-1)
+
+    def move_down(self):
+        self.cur_block.move(1,0)
+        if self.block_inside() == False:
+            self.cur_block.move(-1,0)
+
+    def block_inside(self):
+        tiles = self.cur_block.get_cell_position()
+        for tile in tiles:
+            if self.grid.is_inside(tile.row,tile.column) == False:
+                return False
+        return True
+    
+    def draw(self,screen):
+        self.grid.draw(screen)
+        self.cur_block.draw(screen)
