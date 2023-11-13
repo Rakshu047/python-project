2607fc833a2870ad9930d871a891d46af87b4059
Rakshu047 <poojaryrakshithn@gmail.com>
2023-11-13 13:18:35 +0530

    Checking for collision

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+from msilib.schema import SelfReg
 from grid import Grid
 from blocks import *
 import random
@@ -18,22 +19,37 @@ class Game:
     
     def move_left(self):
         self.cur_block.move(0,-1)
-        if self.block_inside() == False:
+        if self.block_inside() == False or self.block_fits() == False:
             self.cur_block.move(0,1)
     
     def move_right(self):
         self.cur_block.move(0,1)
-        if self.block_inside() == False:
+        if self.block_inside() == False or self.block_fits() == False:
             self.cur_block.move(0,-1)
 
     def move_down(self):
         self.cur_block.move(1,0)
-        if self.block_inside() == False:
+        if self.block_inside() == False or self.block_fits() == False:
             self.cur_block.move(-1,0)
+            self.lock_block()
+
+    def lock_block(self):
+        tiles = self.cur_block.get_cell_position()
+        for pos in tiles:
+            self.grid.grid[pos.row][pos.column] = self.cur_block.id
+        self.cur_block = self.next_block
+        self.next_block = self.get_random_block()
+
+    def block_fits(self):
+        tiles = self.cur_block.get_cell_position()
+        for tile in tiles:
+            if self.grid.is_empty(tile.row, tile.column) == False:
+                return False
+        return True 
 
     def rotate(self):
         self.cur_block.rotate()
-        if self.block_inside() == False:
+        if self.block_inside() == False or self.block_fits() == False:
             self.cur_block.undo_rotate()
 
     def block_inside(self):
